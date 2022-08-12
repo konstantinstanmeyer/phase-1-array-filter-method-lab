48f670447bf1037211292c685109f98241ee2a41
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-08-12 09:24:46 -0600

    commit tree e09a0dc593f2b662faffb69198e2de09140c5754

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+
+function findMatching(input, find){
+    return input.filter(possibleMatch => possibleMatch.toLowerCase() === find.toLowerCase())
+}
+
+function fuzzyMatch(){
+    const result = 0;
+}
+
+function matchName(){
+    const result = 0;
+}
